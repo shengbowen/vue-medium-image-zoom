f25239706c5ca2216604bc271f424875f93d2479
bruce.sheng <shengbowen@meizu.com>
2017-07-23 14:56:44 +0800

    feat: add overlay component

---
+++ src/components/Overlay.vue
@@ -0,0 +1,48 @@
+<template>
+  <div :style="customStyle"></div>
+</template>
+
+<script>
+const transitionDuration = 300;
+
+const defaultStyle = {
+  position: 'absolute',
+  top: 0,
+  right: 0,
+  bottom: 0,
+  left: 0,
+  backgroundColor: '#fff',
+  opacity: 0,
+  transition: `opacity ${ transitionDuration }`,
+};
+
+export default {
+  props: {
+    isVisible: {
+      type: Boolean,
+      required: true,
+    },
+    defaultStyle: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  // mounted() {
+  //   this.isVisible = true;
+  // },
+
+  computed: {
+    customStyle() {
+      const opacity = this.isVisible & 1; // convert bool to 0 or 1
+      return Object.assign(
+        {},
+        defaultStyle,
+        { opacity },
+        this.defaultStyle,
+      );
+    },
+  },
+};
+</script>
+
